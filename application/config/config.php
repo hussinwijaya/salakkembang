538930cb00fdd3604e3aedc2cccdf8d016bdbd76
hussin.wijaya <hussinwijaya@gmail.com>
2020-05-25 12:33:48 +0700

    ke 3

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/caringinnunggal/';
+$config['base_url']	= 'http://localhost/salakkembang/';
 
 /*
 |--------------------------------------------------------------------------
